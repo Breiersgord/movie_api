ad879203277a4dc90baac34bae74a59112c442a9
Breiersgord <128760416+Breiersgord@users.noreply.github.com>
2023-11-22 18:29:49 -0600

    removed 'const' tag from server.js
    removed the 'const' tag from 'http = require' in the server.js file

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const http = require('http'); //creates a variable called 'http' then assigns to it an instance of the HTTP module. this is what imports the HTTP module and allows you to use its function createServer()
-fs = require('fs');
+http = require('http'), //creates a variable called 'http' then assigns to it an instance of the HTTP module. this is what imports the HTTP module and allows you to use its function createServer()
+fs = require('fs'),
 url = require('url');
 
 http.createServer((request, response) => { //2 arguments, request & response; this function will be called every time an HTTP request is made against that server, which why it's called the 'request handler'
     let addr = request.url,
         q = new URL(addr, 'http://localhost:8080'),
-
         filePath = '';
+
             if (q.pathname.includes('documentation')) {
                 filePath = (__dirname + '/documentation.html');
             } else {
@@ -26,9 +26,11 @@ http.createServer((request, response) => { //2 arguments, request & response; th
                     throw err;
                 }
         
-        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write(data);
         response.end('Hello Node!\n');
     });
-}).listen(8080); // port 80 is the standard fort for the HTTP, but you can use any number you want with the following caveat: the port number must be >1024; this is because ports lower than this are reserved by the operating system
+})
+
+.listen(8080); // port 80 is the standard fort for the HTTP, but you can use any number you want with the following caveat: the port number must be >1024; this is because ports lower than this are reserved by the operating system
 console.log('My first Node test server is running on Port 8080.');
