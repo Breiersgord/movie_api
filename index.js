13223b503e20713e98d141198215c0010319bade
Breiersgord <128760416+Breiersgord@users.noreply.github.com>
2023-12-07 19:58:20 -0600

    updates to index.js & public folder
    created public folder and put documentation.html inside
    
    in index.js; added express.static to read documentation.html & added morgan for logging

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
+    fs = require('fs'),
+    path = require('path');
+
 const app = express();
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
 let topTenHorrorMovies = [
     {
@@ -44,21 +50,25 @@ let topTenHorrorMovies = [
     },
   ];
 
+app.use(morgan('combined', {stream: accessLogStream}));
+
+app.use(express.static('public')); //routes all requests for static files to their corresponding files within the 'public' folder on a server
+
   // GET requests
+  
+//app.get('/documentation.html', (req, res)=>{
+    //res.sendFile('public/documentation.html', {root: __dirname});
+//});
+
 app.get('/', (req, res) => {
     res.send('default text response'); //sends a response of various types
-  });
+});
   
-  //app.get('/documentation', (req, res) => {                  
-    //res.sendFile('public/documentation.html', { root: __dirname }); //sends an HTML file to the browser
-  //});
-  
-  app.get('/movies', (req, res) => {
+app.get('/movies', (req, res) => {
     res.json(topTenHorrorMovies); //sends a JSON response
-  });
-  
+});
   
   // listen for requests
-  app.listen(8080, () => {
+app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
-  });
+});
