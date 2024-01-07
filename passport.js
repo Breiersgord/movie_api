a22e5b60291184c72dbec80ebe56133985a57f17
Breiersgord <128760416+Breiersgord@users.noreply.github.com>
2024-01-07 01:34:39 -0600

    corrected erroneous code
    updated 'passportJWT' to 'passport-jwt'

--- passport.js
+++ passport.js
@@ -1,11 +1,11 @@
 const passport = require('passport'),
     LocalStrategy = require('passport-local').Strategy,
     Models = require('./models.js'),
-    passportJWT = require('passportJWT');
+    passportJWT = require('passport-jwt');
 
 let Users = Models.User,
     JWTStrategy = passportJWT.Strategy,
-    ExtractJWT = passportJWT.ExtractJWT;
+    ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(
     new LocalStrategy(
